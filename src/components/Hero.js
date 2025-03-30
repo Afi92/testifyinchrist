b925f78156016808c1fab3a68e20f3f5520d1e87
Afi92 <akpedze.dzreke@proton.me>
2025-03-30 19:18:11 +0200

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '24px' }}>
+      <strong>Jesus saves. Jesus loves. Jesus is the way.</strong>
+    </div>
+  );
+}
+
+export default Hero;
